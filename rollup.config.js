46e7aa7cffd899d2fd78d4b7a0d44e9bbeb8963d
zendabor <yumedzi@gmail.com>
2021-12-01 19:35:04 +0300

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import { babel } from '@rollup/plugin-babel';
+import styles from "rollup-plugin-styles";
+import image from 'rollup-plugin-img';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload'
+
+export default {
+    input: './script.js',
+    output: {
+        file: './build/bundle.js',
+        format: 'cjs',
+    },
+    plugins: [babel({ babelHelpers: 'bundled' }),
+        styles(),
+        image({
+            limit: 100000,
+        }),
+        serve({
+            open: true,
+            contentBase: './',
+            port: 9000,
+        }),
+        livereload(),
+    ]
+};
---
+++ script.js
@@ -0,0 +1,7 @@
+import './style.css';
+console.log('Hello World!');
+
+const img = document.createElement('img');
+img.className = 'img__js';
+img.src = './assets/1024px-Unofficial_JavaScript_logo_2.svg.png'
+document.body.append(img)
